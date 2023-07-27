ee2e9280e6d9c3f669cc2ce4e62f466f66bc3512
Watto100 <joshua.watson6@griffithuni.edu.au>
2023-07-27 10:08:06 +1000

    fixed merging

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function sumn (n1, n2){
     return n1 + n2;
 }
@@ -6,8 +5,5 @@ function sumn (n1, n2){
 console.log("Hello, World!");
 
 console.log(sumn(5, 10));
-=======
-console.log("Hello, World!");
 
 console.log("merge conflict");
->>>>>>> 4e6f817651b503826eb5843bc3d68bc34a5d3f56
